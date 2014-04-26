3d829150bb42de7ca97202dff1052393c9ae97db
Nicholas Reiter <nreiter@cs.washington.edu>
2014-04-25 21:28:27 -0700

    Adding in basic js to v0.0001 of template

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<p style='background:#F00;'>Hello world!</p>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Facebook Hack-a-thon</title>
+    <link href="style.css" rel="stylesheet" type="text/css" media="all" />
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js">
+    </script>
+    <script src="fetch_next_question.js">
+    </script>
+  </head>
+
+  <body>
+    <div id="content">
+      <div id="question">
+        <span>Question #X:</span>
+        <p>Which friend has been to <strong>Paris</strong>?</p>
+      </div>
+      <div id="answers">
+        <ul>
+          <li><label><input type="radio" name="selection" value="0" />Roee Avnon</label></li>
+          <li><label><input type="radio" name="selection" value="1" />Sonja Khan</label></li>
+          <li><label><input type="radio" name="selection" value="2" />Nicholas Reiter</label></li>
+          <li><label><input type="radio" name="selection" value="3" />Colin Miller</label></li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
