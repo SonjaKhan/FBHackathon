bef31af801d0b055fd684f13adc66406d3d5bc46
Sonja Khan <sonjak3@uw.edu>
2014-04-25 20:28:27 -0700

    whoops. here's the file :/

--- question.php
+++ question.php
@@ -1,17 +1,49 @@
 <?php
-
-require_once("php-sdk/facebook.php");
+echo 'nothing done yet';
+require_once('php-sdk/facebook.php');
 
 $config = array(
-      'appId' => '649041091809661',
-      'secret' => '8a01b28cb5e95f6dbf6c0871a53f73ac',
-      'fileUpload' => false, // optional
-      'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
+	'appId' => '649041091809661',
+  'secret' => '8a01b28cb5e95f6dbf6c0871a53f73ac',
+  'fileUpload' => false, // optional
+  'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
 );
-
+echo 'Have not made fb object yet';
 $facebook = new Facebook($config);
+echo 'made fb object';
 
 // Get User ID
-$user = $facebook->getUser();
+$user_id = $facebook->getUser();
+echo $user_id;
+
+if ($user_id) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me','GET');
+    $namePrint = "Name: " . $user_profile['name'];
+  } catch (FacebookApiException $e) {
+    // If the user is logged out, you can have a 
+    // user ID even though the access token is invalid.
+    // In this case, we'll get an exception, so we'll
+    // just ask the user to login again here.
+    $login_url = $facebook->getLoginUrl(); 
+    echo 'Please <a href="' . $login_url . '">login.</a>';
+    error_log($e->getType());
+    error_log($e->getMessage());
+  }
+} else {
+	// No user, print a link for the user to login
+	$login_url = $facebook->getLoginUrl();
+  echo 'Please <a href="' . $login_url . '">login.</a>';
+}
+
 
 ?>
+<!DOCTYPE HTML>
+<head>
+<title>TEST</title>
+</head>
+<body>
+	<p><?php echo $namePrint?></p>
+</body>
+</html>
