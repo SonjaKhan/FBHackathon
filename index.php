37eb92230adcb49d9c0144ba82f16186f32eb3d5
Nicholas Reiter <nreiter@cs.washington.edu>
2014-04-25 23:25:34 -0700

    Wrapping login status

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <script>
     $(document).on('click', '#login', function(e) {
       e.preventDefault();
-      FB.login();//function(){}, {scope: 'friends'});
+      FB.getLoginStatus(checkLoginStatus);
     });
 
     window.fbAsyncInit = function() {
@@ -30,18 +30,25 @@
       // for any authentication related change, such as login, logout or session refresh. This means that
       // whenever someone who was previously logged out tries to log in again, the correct case below 
       // will be handled. 
-      FB.Event.subscribe('auth.authResponseChange', function(response) {
-        // Here we specify what we do with the response anytime this event occurs. 
+      FB.Event.subscribe('auth.authResponseChange', checkLoginStatus);
+      };
+
+      // Load the SDK asynchronously
+      (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+      }(document));
+
+      function checkLoginStatus(response) {
         if (response.status === 'connected') {
           // The response object is returned with a status field that lets the app know the current
           // login status of the person. In this case, we're handling the situation where they 
           // have logged in to the app.
           $('#content').css('display', 'block');
-
-          $('#logout').click(function(e) {
-            e.preventDefault();
-            FB.logout();
-          });
+          $('#login').css('display', 'none');
 
         } else if (response.status === 'not_authorized') {
           // In this case, the person is logged into Facebook, but not into the app, so we call
@@ -60,17 +67,7 @@
           // The same caveats as above apply to the FB.login() call here.
           FB.login();
         }
-      });
-      };
-
-      // Load the SDK asynchronously
-      (function(d){
-        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement('script'); js.id = id; js.async = true;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        ref.parentNode.insertBefore(js, ref);
-      }(document));
+      }
 
     </script>
 
