b13d52de485d2bb68fddcdef4ae8db7f2a510728
ravnon <avnon@uw.edu>
2014-04-26 02:04:09 -0700

    fixed bug?

--- question.php
+++ question.php
@@ -74,10 +74,9 @@ function getHometownQuestion($facebook) {
   $answersUID = array($questionUID);
   for ($i = 0; $i < 3; $i++) {
     $j = rand(0, count($hometownToPeople) - 1);
-    $answerCity = $hometownToPeople[$j];
-    $k = rand(0, count($answerCity) - 1);
-    $answerPerson = $answerCity[$k];
-    array_push($answersUID, $answerPerson);
+    $randomCity = array_rand($hometownToPeople);
+    $randomUIDInCity = array_rand($hometownToPeople[$randomCity]);
+    array_push($answersUID, $randomUIDInCity);
   }
 
   $questionHometown = $questionCity . " (" . $questionCountry . ")";
