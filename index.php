1d79441d9746118beb17784fc9ad5fd424cb482e
Nicholas Reiter <nreiter@cs.washington.edu>
2014-04-25 22:09:14 -0700

    Maybe fixing it this time

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <title>Facebook Hack-a-thon</title>
     <link href="style.css" rel="stylesheet" type="text/css" media="all" />
-    <script src="http://connect.facebook.net/en_US/all.js">
+    <script src="https://connect.facebook.net/en_US/all.js">
     </script>
     <script src="https://code.jquery.com/jquery-1.11.0.min.js">
     </script>
