f9ae06adad8bda3a1599f54dc774065a1376edac
Colin Miller <cjmiller@uw.edu>
2014-04-26 08:47:46 -0700

    parens

--- questions.js
+++ questions.js
@@ -29,7 +29,7 @@ $(window).load(function() {
 	$("#answers").html("");
 	$("#question").html("");
     $("#question").append($("<span>").html("Summary"));
-	$("#question").append($("<div>").attr("id", "finalPercent").html("Total: " + Math.floor((score / (questionNumber - 1) * 100) + "%")));
+	$("#question").append($("<div>").attr("id", "finalPercent").html("Total: " + Math.floor(score / (questionNumber - 1) * 100) + "%"));
 	$("#question").append($("<p>").html("Of 15 questions about your friends, you got " + score + "!"));
 	$("#question").append($("<a>").attr("id", "postScores").attr("href","#").html("Click here to share your score"));
 	$("#question").append($("<a>").attr("href","#").html("Click here to play again"));
